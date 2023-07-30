b67c6f60ef761e329df07e3dfcaf20797e06db91
BigCubeCat <bigcubecat@yandex.ru>
2023-07-31 00:00:01 +0700

    feat(dto): add filter dto

--- src/filter.ts
+++ src/filter.ts
@@ -1,6 +1,7 @@
 import {TLog} from './log';
+import {type} from 'os';
 
-export type Filter = {
+export type FilterDto = {
 	applied: boolean;
 	level: {
 		debug: boolean;
@@ -16,13 +17,22 @@ export type Filter = {
 	syscallname: string;
 };
 
-export const defaultFilter: Filter = {
+export type TFilter = {
+	applied: boolean;
+	levels: Array<string>;
+	types: Array<string>;
+	prefix: RegExp;
+	taskname: RegExp;
+	syscallname: RegExp;
+};
+
+export const defaultFilter: TFilter = {
 	applied: false,
-	level: {debug: true, warning: true, info: true},
-	type: {enter: true, exit: true},
-	prefix: '',
-	taskname: '',
-	syscallname: '',
+	levels: ['debug', 'info', 'warning'],
+	types: ['E', 'X'],
+	prefix: new RegExp(''),
+	taskname: new RegExp(''),
+	syscallname: new RegExp(''),
 };
 
 const regexIsValid = (regex: string) => {
@@ -35,34 +45,42 @@ const regexIsValid = (regex: string) => {
 	}
 };
 
-export function checkSuggest(log: TLog, filter: Filter) {
-	// TODO not parse regex every check
-	if (filter.prefix && regexIsValid(filter.prefix)) {
-		const prefixRegex = new RegExp(filter.prefix);
-		if (!prefixRegex.test(log.LogPrefix)) return false;
+export const toTFilter = (dto: FilterDto) => {
+	let levels: string[] = [];
+	let types: string[] = [];
+	if (dto.level.info) levels.push('info');
+	if (dto.level.debug) levels.push('debug');
+	if (dto.level.warning) levels.push('warning');
+	if (dto.type.exit) types.push('X');
+	if (dto.type.enter) types.push('E');
+	if (levels.length === 0) {
+		levels = ['info', 'debug', 'warning'];
 	}
-	if (filter.taskname && regexIsValid(filter.taskname)) {
-		const tasknameRegex = new RegExp(filter.taskname);
-		if (!tasknameRegex.test(log.Taskname)) return false;
+	if (types.length === 0) {
+		types = ['E', 'X'];
 	}
-	if (filter.syscallname && regexIsValid(filter.syscallname)) {
-		const syscallnameRegex = new RegExp(filter.syscallname);
-		if (!syscallnameRegex.test(log.Syscallname)) return false;
-	}
+	return {
+		applied: dto.applied,
+		levels: levels,
+		types: types,
+		prefix: regexIsValid(dto.prefix)
+			? new RegExp(dto.prefix)
+			: new RegExp(''),
+		taskname: regexIsValid(dto.taskname)
+			? new RegExp(dto.taskname)
+			: new RegExp(''),
+		syscallname: regexIsValid(dto.syscallname)
+			? new RegExp(dto.syscallname)
+			: new RegExp(''),
+	};
+};
 
-	if (log.LogType == 'E') {
-		if (!filter.type.enter) return false;
-	} else {
-		if (!filter.type.exit) return false;
-	}
-
-	if (log.level == 'info') {
-		if (!filter.level.info) return false;
-	} else if (log.level == 'debug') {
-		if (!filter.level.debug) return false;
-	} else if (log.level == 'warning') {
-		if (!filter.level.warning) return false;
-	}
-
-	return true;
+export function checkSuggest(log: TLog, filter: TFilter) {
+	return (
+		filter.levels.includes(log.level) &&
+		filter.types.includes(log.LogType) &&
+		filter.prefix.test(log.LogPrefix) &&
+		filter.taskname.test(log.Taskname) &&
+		filter.syscallname.test(log.Syscallname)
+	);
 }
